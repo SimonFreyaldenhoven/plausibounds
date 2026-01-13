a0a56796376a5772822e321f6728dba860a62e13
Ryan Kobler <kobleary@gmail.com>
2026-01-13 19:48:31 +0000

    #9 tweak DESCRIPTION to avoid "Implements" "Functions for..." language

--- R/plausibounds-package.R
+++ R/plausibounds-package.R
@@ -1,6 +1,6 @@
 #' plausibounds: Plausible Bounds for Treatment Path Estimates
 #'
-#' Implements the additions to dynamic effect plots suggested in Freyaldenhoven and Hansen (2026). 
+#' Enhances dynamic effect plots as suggested in Freyaldenhoven and Hansen (2026). 
 #' Data-driven smoothing delivers a smooth estimated path with potentially improved point estimation properties and confidence regions covering a surrogate that can be substantially tighter than conventional pointwise or uniform bands.
 #'
 #' @section Main Functions:
